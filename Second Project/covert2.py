01bbd4e1efa5d0ab38a94c12c585ca29577d018a
mcw1217 <mcw1217@naver.com>
2022-01-03 09:45:38 +0900

    .

--- Second Project/covert2.py
+++ Second Project/covert2.py
@@ -3,9 +3,9 @@ from time import sleep
 
 with open("./send_logo.png", "rb") as f:
     while True:
-      byte=f.read(1024)
-      if byte==b"": #EOF, NULL
-          ping("192.168.219.105", verbose=True, count=1, payload=b"EOF")
-          break
-      ping("192.168.219.105", verbose=True, count=1, payload=byte)
-      sleep(0.5)
+        byte = f.read(1024)
+        if byte == b"":  # EOF, NULL
+            ping("192.168.219.105", verbose=True, count=1, payload=b"EOF")
+            break
+        ping("192.168.219.105", verbose=True, count=1, payload=byte)
+        sleep(0.5)
